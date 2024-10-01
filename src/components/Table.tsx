c54c32f5f1e226de4b88ba515746bc29fb4fcc4f
Eugene <eugeneskom@gmail.com>
2024-10-01 16:17:18 +0300

    added carrier list table and rendered the data from db

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,6 +1,9 @@
+"use client";
 // Import necessary React and Next.js types
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { CarrierData } from "@/types";
+import { useState } from "react";
+import { Button } from "./ui/button";
 
 // Define the props interface that will be passed to your page component
 interface TableProps {
@@ -9,6 +12,33 @@ interface TableProps {
 
 // Define the TableCarriers component to render the table
 function TableCarriers({ data }: TableProps) {
+  const [editingRow, setEditingRow] = useState<number | null>(null);
+  const [editingData, setEditingData] = useState<CarrierData>({
+    carrier_number: "",
+    agent_number: "",
+    home_city: "",
+    carrier_email: "",
+    mc_number: "",
+    company_name: "",
+    company_phone: "",
+    truck_type_spam: "",
+    spam: false,
+  });
+
+  const setEditingRowHandler = (index: number) => {
+    setEditingRow(index);
+    setEditingData(data[index]);
+  };
+  const handleEditingData = (index: number, key: string, value: string | boolean) => {
+    setEditingData((prevState: CarrierData) => {
+      return {
+        ...prevState,
+        [key]: value,
+      };
+    });
+  };
+
+  console.log(editingData);
 
   return (
     <Table>
@@ -29,17 +59,32 @@ function TableCarriers({ data }: TableProps) {
       <TableBody>
         {/* Iterate over the data array and render each row */}
         {data.map((item, index) => (
-          <TableRow key={index}>
-            <TableCell>{item.carrier_number}</TableCell>
-            <TableCell>{item.agent_number}</TableCell>
-            <TableCell>{item.home_city}</TableCell>
-            <TableCell>{item.carrier_email}</TableCell>
-            <TableCell>{item.mc_number}</TableCell>
-            <TableCell>{item.company_name}</TableCell>
-            <TableCell>{item.company_phone}</TableCell>
-            <TableCell>{item.truck_type_spam}</TableCell>
-            <TableCell>{item.spam}</TableCell>
-          </TableRow>
+          <>
+            {editingRow === index ? (
+              Object.entries(item).map(([key, value]) => {
+                return (
+                  <TableRow key={key}>
+                    <TableCell key={key} className={`${index % 2 === 0 ? "" : "bg-slate-300 hover:bg-slate-50"}`}>
+                      <input
+                        type={typeof value === "boolean" ? "checkbox" : "text"} // Correct type checking
+                        defaultChecked={typeof value === "boolean" ? value : undefined} // Handle checkbox for boolean
+                        value={typeof value !== "boolean" ? String(value) : undefined} // Handle string values
+                      />
+                    </TableCell>
+                  </TableRow>
+                );
+              })
+            ) : (
+              <TableRow key={index} className={`${index % 2 === 0 ? "" : "bg-slate-300 hover:bg-slate-50"}`}>
+                {Object.entries(item).map(([key, value]) => {
+                  return <TableCell key={key}>{value}</TableCell>;
+                })}
+                <TableCell>
+                  <Button onClick={() => setEditingRowHandler(index)}>Edit</Button>
+                </TableCell>
+              </TableRow>
+            )}
+          </>
         ))}
       </TableBody>
     </Table>
