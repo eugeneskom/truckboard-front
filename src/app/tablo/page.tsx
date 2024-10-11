da7bac06e212819f1a90f5c727e2a13122678cc9
eugeneskom <eugeneskom@gmail.com>
2024-10-12 01:35:57 +0300

    finished websocket set up for aggregated page

--- src/app/tablo/page.tsx
+++ src/app/tablo/page.tsx
@@ -2,42 +2,70 @@
 
 import AggregatedDataTable from "@/components/tables/aggregated/AggregatedTable";
 import { SearchRateType } from "@/types";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
+import { useWebSocket } from "@/components/WebSocketProvider";
 
 async function handleGetAggregatedData(): Promise<SearchRateType[]> {
   const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}api/aggregated`, {
     cache: "no-cache",
   });
-
-  // console.log(`${process.env.NEXT_PUBLIC_SERVER_URL}`, res);
-  // if (!res.ok) {
-  //   throw new Error("Failed to fetch data");
-  // }
-
   return res.json();
 }
 
- function TabloPage() {
+function TabloPage() {
   const [data, setData] = useState<SearchRateType[]>([]);
+  const { socket } = useWebSocket();
+
+  const fetchData = useCallback(async () => {
+    const res: SearchRateType[] = await handleGetAggregatedData();
+    setData(res);
+  }, []);
 
   useEffect(() => {
-  // const res: PostingTypes[] = await handleGetAggregatedData(); // Fetch data server-side
-  // setData(res)
+    fetchData();
+  }, [fetchData]);
 
-  const handleData = async () => {
-    const res: SearchRateType[] = await handleGetAggregatedData(); // Fetch data server-side
-    setData(res);
-  }
+  useEffect(() => {
+    if (!socket) return;
 
-  handleData()
+    const handleWebSocketMessage = (event: MessageEvent) => {
+      const message = JSON.parse(event.data);
+      console.log('WebSocket message received:', message);
 
-  }, []);
+      switch (message.type) {
+        case 'update':
+          setData(prevData => 
+            prevData.map(item => 
+              item.search_id === message.data.id 
+                ? { ...item, [message.data.field]: message.data.value }
+                : item
+            )
+          );
+          break;
+        case 'add':
+          setData(prevData => [...prevData, message.data]);
+          break;
+        case 'delete':
+          setData(prevData => 
+            prevData.filter(item => item.search_id !== message.data.id)
+          );
+          break;
+        default:
+          console.log('Unknown message type:', message.type);
+      }
+    };
+
+    socket.addEventListener('message', handleWebSocketMessage);
+
+    return () => {
+      socket.removeEventListener('message', handleWebSocketMessage);
+    };
+  }, [socket]);
 
   return (
     <div className="px-5">
       <p>TabloPage #1</p>
       <AggregatedDataTable data={data} setData={setData}/>
- 
     </div>
   );
 }
